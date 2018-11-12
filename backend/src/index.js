d8c09361b8eb21981e25652fa0240d73068903d3
Jonatan Lins <jonatanpinheiro@outlook.com>
2018-11-12 17:46:55 -0300

    configuração de porta do servidor

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,6 +8,8 @@ const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 
+const port = process.env.PORT || 5000
+
 mongoose.connect(
   'mongodb://goweek:goweek123@ds155213.mlab.com:55213/goweek-4',
   { useNewUrlParser: true }
@@ -21,6 +23,6 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-server.listen(3000, () => {
-  console.log('Server is running on port 3000')
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
 })
